778bcff16826660572f417ca2aee95f1f5fab647
trimurti-dora <40144801+trimurti-dora@users.noreply.github.com>
2018-06-10 21:56:09 +0530

    To test pull

--- DemoRepo/WebDriverPrograms/src/automationTesting/ParameterTest.java
+++ DemoRepo/WebDriverPrograms/src/automationTesting/ParameterTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class ParameterTest {
 	WebDriver driver;
 	
