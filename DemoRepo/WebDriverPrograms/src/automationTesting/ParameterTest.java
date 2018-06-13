00896d84f75dc823ace0a5877677d66f6e1b580d
trimurti-dora <trimurti1992@gmail.com>
2018-06-14 00:40:25 +0530

    testing jenkins auto trigger

--- DemoRepo/WebDriverPrograms/src/automationTesting/ParameterTest.java
+++ DemoRepo/WebDriverPrograms/src/automationTesting/ParameterTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class ParameterTest {
 	WebDriver driver;
 	
